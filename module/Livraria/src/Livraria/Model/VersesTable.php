42ca363ac4178a794a6be17ac961d5750d60dbb7
Gabriel Goncalves <gabrielll_07@hotmail.com>
2015-01-24 01:07:48 -0200

    retirando funcoes para setar a column e o quantifier

--- module/Livraria/src/Livraria/Model/VersesTable.php
+++ module/Livraria/src/Livraria/Model/VersesTable.php
@@ -10,22 +10,12 @@ use Zend\Db\Adapter\Adapter,
 class VersesTable extends AbstractTableGateway{
     
     protected $table = 'verses';
-    protected $columns = ['*'];
-    protected $quantifier;
     
     public function __construct($adapter) {
         $this->adapter = $adapter;
         $this->resultSetPrototype = new ResultSet();
         $this->resultSetPrototype->setArrayObjectPrototype(new Verses());
         $this->initialize();
-    }
-    
-    public function setColumns(Array $data){
-        $this->columns = $data;
-    }
-    
-    public function setQuantifier($data){
-        $this->quantifier = $data;
     }
     
 }
